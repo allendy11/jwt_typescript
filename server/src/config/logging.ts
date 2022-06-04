a009256866fd0795406fd365704f49f8af9341e9
Daehee Yoon <allendy11@gmail.com>
2022-06-05 00:09:34 +0900

    [server] add error_log in logging.ts

--- server/src/config/logging.ts
+++ server/src/config/logging.ts
@@ -1,6 +1,10 @@
 const info = (namespace: string, message: string) => {
   // console.info(`[${getTimeStamp()}] [INFO] [${namespace}] [${message}]`);
-  console.info(`[${namespace}] [${message}]`);
+  console.info(`INFO: [${namespace}] [${message}]`);
+};
+const error = (namespace: string, message: string) => {
+  // console.info(`[${getTimeStamp()}] [ERROR] [${namespace}] [${message}]`);
+  console.info(`ERROR: [${namespace}] [${message}]`);
 };
 
 const getTimeStamp = (): string => {
